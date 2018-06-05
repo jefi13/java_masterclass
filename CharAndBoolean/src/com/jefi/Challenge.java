68876aa17c320ab2259a85c8f2624979f32cf518
JelenaMarjanovic <jelena.marjanovic.13@gmail.com>
2018-06-05 23:15:08 +0200

    4.19. Char and Boolean

---
+++ CharAndBoolean/src/com/jefi/Challenge.java
@@ -0,0 +1,16 @@
+package com.jefi;
+
+public class Challenge {
+
+    public static void main(String[] args) {
+
+        // 1. Find the for the registered symbol on the same line as the copyright symbol.
+        // 2. Create a variable of type char and assign it the unicode value for that symbol.
+        // 3. Display it on screen.
+
+        char registeredSymbol = '\u00AE';
+
+        System.out.println("Registered symbol: " + registeredSymbol);
+    }
+
+}
