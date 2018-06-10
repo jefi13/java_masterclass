1ad2e789c48aede1439bbfe9927a65a9a36ebd68
JelenaMarjanovic <jelena.marjanovic.13@gmail.com>
2018-06-10 19:37:18 +0200

    5.24. Keywords and Expressions

---
+++ KeywordsAndExpressions/src/com/jefi/Challenge.java
@@ -0,0 +1,19 @@
+package com.jefi;
+
+public class Challenge {
+
+    public static void main(String[] args) {
+
+        // In the following code that I will type below, write down what parts of the code
+        // are expressions.
+
+        int score = 100;        // score = 100
+
+        if (score > 99) {       // score > 99
+            System.out.println("You got the high score!");  // "You got the high score!"
+            score = 0;          // score = 0
+        }
+
+    }
+
+}
