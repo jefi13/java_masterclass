40ca2c08061070a4aa3bce78d5adcc78e2fe6dd7
JelenaMarjanovic <jelena.marjanovic.13@gmail.com>
2018-06-11 02:14:50 +0200

    5.25. Statements, Whitespace and Indentation (Code Organization)

--- StatementsWhiteSpaceAndIndenting/src/com/jefi/Main.java
+++ StatementsWhiteSpaceAndIndenting/src/com/jefi/Main.java
@@ -5,17 +5,17 @@ public class Main {
     public static void main(String[] args) {
         int myVariable = 50;
         if (myVariable == 50) {
-            System.out.println("Printed");
+            System.out.println("Printed.");
         }
 
         myVariable++;
         myVariable--;
-        System.out.println("This is a test");
+        System.out.println("This is a test.");
 
         System.out.println("This is" +
                 " another" +
                 " still more.");
 
-        int anotherVariable = 50;myVariable--;System.out.println("This is another one");
+        int anotherVariable = 50;myVariable--;System.out.println("This is another one.");
     }
 }
